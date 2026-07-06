393afde77eed5983bad7768e76b264a110111e04
Nicholas Carrasquilla <carrasquilla.nicholas@gmail.com>
2026-07-06 14:16:22 -0400

    Added main functionallity

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
 from parser import parse_input
-from matching import stable_matching
+from matching import match_residents
 from stability import check_stability
+import sys
+
 
 def main():
     print("CS4306 Assignment 1")
+    print("Paste input, then press Ctrl+Z then Enter on Windows:")
+
+    text = sys.stdin.read()
+
+    hospitals, residents = parse_input(text)
+    matches = match_residents(hospitals, residents)
+
+    print("\nMatching:")
+    for hospital, assigned_residents in matches.items():
+        print(hospital + ", " + ", ".join(assigned_residents))
+
+    stable = check_stability(hospitals, residents, matches)
+    print("\nStable:", stable)
+
 
 if __name__ == "__main__":
     main()
