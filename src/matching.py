7ebfc14e901190c7b38ea7fadd2f5b90ffbaf153
Nicholas Carrasquilla <carrasquilla.nicholas@gmail.com>
2026-07-06 13:21:32 -0400

    Added structure for algorithm

--- src/matching.py
+++ src/matching.py
@@ -1,2 +1,18 @@
 def match_residents(hospitals, residents):
-    pass
+    
+    # All residents start unmatched
+    free_residents = list(residents.keys())
+
+    # Each resident starts at the first hospital in their preference list
+    next_choice = {}
+    for resident in residents:
+        next_choice[resident] = 0
+    
+    # Each hospital starts with no matched residents
+    matches = {}
+    for hospital in hospitals:
+        matches[hospital] = []
+    
+    # Loop for checking each residents proposals to hospitals
+    while len(free_residents) > 0:
+        current_resident = free_residents.pop(0)
